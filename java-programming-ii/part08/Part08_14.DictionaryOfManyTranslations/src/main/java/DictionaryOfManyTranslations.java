260a753a1321394d13691d9f4ccef2d596d2681f
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-20 11:17:10 -0300

    Part08_14: Create DictionaryOfManyTranslations class

---
+++ java-programming-ii/part08/Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class DictionaryOfManyTranslations {
+
+    public void add(String word, String translation) {
+
+    }
+
+    public ArrayList<String> translate(String word) {
+
+
+        return null;
+    }
+
+    public void remove(String word) {
+
+    }
+}
