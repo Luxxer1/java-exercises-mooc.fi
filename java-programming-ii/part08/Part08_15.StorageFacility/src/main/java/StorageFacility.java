a736648ee24954aeb793033341feab5580eb2896
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-21 13:28:10 -0300

    Part08_15: Create StorageFacility class

---
+++ java-programming-ii/part08/Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StorageFacility {
+
+    public StorageFacility() {
+
+    }
+
+    public void add(String unit, String item) {
+
+    }
+
+    public void remove(String storageUnit, String item) {
+
+    }
+
+    public ArrayList<String> contents(String storageUnit) {
+
+
+        return null;
+    }
+
+    public ArrayList<String> storageUnits() {
+
+        return null;
+    }
+}
