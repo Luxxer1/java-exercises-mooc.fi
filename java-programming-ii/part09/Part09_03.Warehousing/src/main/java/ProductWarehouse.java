54fbcf9e7693a1a31244edd298fb169ca5061363
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-23 09:16:49 -0300

    Part09_03: Create ProductWarehouse that extends Warehouse and getName method

---
+++ java-programming-ii/part09/Part09_03.Warehousing/src/main/java/ProductWarehouse.java
@@ -0,0 +1,12 @@
+public class ProductWarehouse extends Warehouse {
+    private final String productName;
+
+    public ProductWarehouse(String productName, double capacity) {
+        super(capacity);
+        this.productName = productName;
+    }
+
+    public String getName() {
+        return productName;
+    }
+}
