0ce4cebbb5990ce98493dda85f3787d3f06b4ff1
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-23 10:51:38 -0300

    Part09_03: Create ProductWarehouseWithHistory class and history method

---
+++ java-programming-ii/part09/Part09_03.Warehousing/src/main/java/ProductWarehouseWithHistory.java
@@ -0,0 +1,14 @@
+public class ProductWarehouseWithHistory extends ProductWarehouse {
+    private ChangeHistory changeHistory;
+
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
+        super(productName, capacity);
+        changeHistory = new ChangeHistory();
+        changeHistory.add(initialBalance);
+        addToWarehouse(initialBalance);
+    }
+
+    public String history() {
+        return changeHistory.toString();
+    }
+}
