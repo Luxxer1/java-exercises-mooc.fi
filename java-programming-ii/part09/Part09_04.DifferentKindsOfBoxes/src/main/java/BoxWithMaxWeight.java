f65d0609cdf2a5a35d17bb4fbf22f783b66f4c43
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-25 10:24:54 -0300

    Part09_04: Create BoxWithMaxWeight class that extends Box and implements add method

---
+++ java-programming-ii/part09/Part09_04.DifferentKindsOfBoxes/src/main/java/BoxWithMaxWeight.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BoxWithMaxWeight extends Box {
+    private int maxWeight;
+    private int currentWeight;
+    private ArrayList<Item> items;
+
+    public BoxWithMaxWeight(int capacity) {
+        this.maxWeight = capacity;
+        this.currentWeight = 0;
+        this.items = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Item item) {
+        if (currentWeight + item.getWeight() <= maxWeight) {
+            items.add(item);
+            currentWeight += item.getWeight();
+        }
+    }
+}
