0e3a3044addc64e1aceb5dd0ebc5200844d3ef53
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-29 13:36:00 -0300

    Part09_06: Create class Box and implements Packable, and add code to test

---
+++ java-programming-ii/part09/Part09_06.InterfaceInABox/src/main/java/Box.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Box implements Packable {
+    private final double maxWeight;
+    private ArrayList<Packable> items;
+
+    public Box(double maxWeight) {
+        this.maxWeight = maxWeight;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(Packable item) {
+        if (weight() + item.weight() <= maxWeight) {
+            items.add(item);
+        }
+    }
+
+    @Override
+    public double weight() {
+        double weight = 0;
+
+        for (Packable item: items) {
+            weight += item.weight();
+        }
+
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Box: " + items.size() + " items, total weight " + weight() + " kg";
+    }
+}
