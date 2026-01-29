07d83d4df8ca677536b415cc282d29ecca3098ac
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-29 13:23:08 -0300

    Part09_06: Create class CD and implements Packable

---
+++ java-programming-ii/part09/Part09_06.InterfaceInABox/src/main/java/CD.java
@@ -0,0 +1,23 @@
+public class CD implements Packable {
+    private final String artist;
+    private final String name;
+    private final int year;
+    private final double weight;
+
+    public CD(String artist, String name, int year) {
+        this.artist = artist;
+        this.name = name;
+        this.year = year;
+        this.weight = 0.1;
+    }
+
+    @Override
+    public double weight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return artist + ": " + name + " (" + year + ")";
+    }
+}
