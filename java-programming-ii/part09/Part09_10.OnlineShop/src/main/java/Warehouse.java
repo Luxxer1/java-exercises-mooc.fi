ca091d142c2a0c0e8fbc90a0e203f149be545c30
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-30 12:07:13 -0300

    Part09_10: Create Warehouse class

---
+++ java-programming-ii/part09/Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Warehouse {
+    Map<String, Integer> prices;
+
+    public Warehouse() {
+        this.prices = new HashMap<>();
+    }
+
+    public void addProduct(String product, int price, int stock) {
+
+    }
+
+    public int price(String product) {
+
+    }
+}
