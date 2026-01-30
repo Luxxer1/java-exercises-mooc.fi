963a7589dd1fad4d195fd0d9827f54f425b832ea
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-30 12:09:42 -0300

    Part09_10: Create stock map

--- java-programming-ii/part09/Part09_10.OnlineShop/src/main/java/Warehouse.java
+++ java-programming-ii/part09/Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -3,9 +3,11 @@ import java.util.HashMap;
 
 public class Warehouse {
     Map<String, Integer> prices;
+    Map<String, Integer> stock;
 
     public Warehouse() {
         this.prices = new HashMap<>();
+        this.stock = new HashMap<>();
     }
 
     public void addProduct(String product, int price, int stock) {
