0fa0dda940e58a986bc77774deb7c689a9b6b9bd
Lucas Bastos <lfsouzabastos@gmail.com>
2026-01-30 10:47:11 -0300

    Part09_09: Add code to test returnSize

--- java-programming-ii/part09/Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ java-programming-ii/part09/Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,10 +1,17 @@
 import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        // You can test your method here
+        Set<String> names = new HashSet<>();
+        names.add("first");
+        names.add("first");
+        names.add("second");
+        names.add("second");
+        names.add("second");
 
+        System.out.println(returnSize(names));
     }
 
     public static int returnSize(Set object) {
