8aa6427838eb0f59ceb274fa78f125e0a7d89943
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-02 09:40:47 -0300

    Part09_10: Refactor shop method to work

--- java-programming-ii/part09/Part09_10.OnlineShop/src/main/java/Store.java
+++ java-programming-ii/part09/Part09_10.OnlineShop/src/main/java/Store.java
@@ -10,7 +10,6 @@ public class Store {
         this.scanner = scanner;
     }
 
-    // the method that handles the customers visit to the store.
     public void shop(String customer) {
         ShoppingCart cart = new ShoppingCart();
         System.out.println("Welcome to the store " + customer);
@@ -27,9 +26,10 @@ public class Store {
                 break;
             }
 
-            // Add code here that adds the product to the cart,
-            // If there is any in the warehouse, and reduces the stock in the warehouse
-            // Don't touch any of the other code!
+
+            if (warehouse.take(product)) {
+                cart.add(product, warehouse.price(product));
+            }
         }
 
         System.out.println("your shoppingcart contents:");
