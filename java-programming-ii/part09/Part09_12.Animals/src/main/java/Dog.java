3b6a88037365f9f59cc928b444cb7f3ac1575332
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-02 11:57:11 -0300

    Part09_12: Create Dog class and Implements NoiseCapable

---
+++ java-programming-ii/part09/Part09_12.Animals/src/main/java/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal implements NoiseCapable {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public Dog() {
+        super("Dog");
+    }
+
+    @Override
+    public void makeNoise() {
+        bark();
+    }
+}
