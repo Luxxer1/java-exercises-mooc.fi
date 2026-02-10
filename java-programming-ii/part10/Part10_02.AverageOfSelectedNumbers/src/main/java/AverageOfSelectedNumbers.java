aac1b60ed29246d62dd8f8fab0a44048286a7b48
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-10 08:24:40 -0300

    Part10_02: Implement exercise

--- java-programming-ii/part10/Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ java-programming-ii/part10/Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,44 @@
-
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> inputs = new ArrayList<>();
 
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String choice = scanner.nextLine();
+
+        System.out.println("Average of the positive numbers: " + getAverage(inputs, choice));
+    }
+
+    public static double getAverage(List<String> inputs, String choice) {
+        IntStream stream = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s));
+
+        switch (choice) {
+            case "n":
+                stream = stream.filter(number -> number < 0);
+                break;
+
+            case "p":
+                stream = stream.filter(number -> number >= 0);
+                break;
+        }
+
+        return stream.average().getAsDouble();
     }
 }
