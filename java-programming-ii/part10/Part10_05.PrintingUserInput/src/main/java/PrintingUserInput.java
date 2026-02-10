6527757f15870f7382d2f11fe9c3be2d69746522
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-10 09:39:35 -0300

    Part10_05: Implement exercise

--- java-programming-ii/part10/Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ java-programming-ii/part10/Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,4 +1,4 @@
-
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,6 +6,18 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
 
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        inputs.stream()
+                .forEach(input -> System.out.println(input));
     }
 }
