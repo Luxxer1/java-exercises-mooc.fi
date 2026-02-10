2081190cd73b283b0559d20d317051c268dde375
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-10 11:10:23 -0300

    Part10_08: Refactor printItems to remove loop and implement stream

--- java-programming-ii/part10/Part10_08.Weighting/src/main/java/Suitcase.java
+++ java-programming-ii/part10/Part10_08.Weighting/src/main/java/Suitcase.java
@@ -26,12 +26,7 @@ public class Suitcase {
     }
 
     public void printItems() {
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        Item t = this.Items.get(indeksi);
-        System.out.println(t);
-        indeksi++;
-        }
+        Items.stream().forEach(System.out::println);
     }
 
     public Item heaviestItem() {
