7589e35d8e6e5989a1779044fc570d574a4b1363
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-11 10:54:38 -0300

    Part10_13: Implement sorted by literacy percent from file csv

--- java-programming-ii/part10/Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ java-programming-ii/part10/Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -7,6 +7,18 @@ import java.util.ArrayList;
 public class LiteracyComparison {
     
     public static void main(String[] args) {
+        String fileName = "literacy.csv";
 
+        try {
+            Files.lines(Paths.get(fileName))
+                    .sorted((p1, p2) ->
+                            Double.valueOf(p1.split(",")[5]).compareTo(Double.valueOf(p2.split(",")[5]))
+                    ).forEach(row -> {
+                        String[] rowSplited = row.split(",");
+                        System.out.println(rowSplited[3] + " (" + rowSplited[4] + "), " + rowSplited[2].split(" ")[1] + ", " + rowSplited[5]);
+                    });
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
