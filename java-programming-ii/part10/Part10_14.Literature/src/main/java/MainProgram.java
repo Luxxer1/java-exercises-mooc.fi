6cd1c7142319f696914fc14e15c329bdd3eb7413
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-11 11:59:43 -0300

    Part10_14: Create code to register inputs of the user as books in a list

--- java-programming-ii/part10/Part10_14.Literature/src/main/java/MainProgram.java
+++ java-programming-ii/part10/Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,28 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Book> books = new ArrayList<>();
 
+        while (true) {
+            System.out.println("Input the name of the book, empty stops: ");
+            String inputName = scanner.nextLine();
+
+            if (inputName.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Input the age recommendation: ");
+            int inputAge = Integer.parseInt(scanner.nextLine());
+
+            books.add(new Book(inputName, inputAge));
+        }
     }
 
 }
