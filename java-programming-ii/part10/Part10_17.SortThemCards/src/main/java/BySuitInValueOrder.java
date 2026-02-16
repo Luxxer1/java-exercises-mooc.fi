fc120ea752c2b07fa9b55199bdfc425c19d1c3e2
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-16 09:10:15 -0300

    Part10_17: Refactor BySuitInValueOrder method for better performance

--- java-programming-ii/part10/Part10_17.SortThemCards/src/main/java/BySuitInValueOrder.java
+++ java-programming-ii/part10/Part10_17.SortThemCards/src/main/java/BySuitInValueOrder.java
@@ -2,12 +2,12 @@ import java.util.Comparator;
 
 public class BySuitInValueOrder implements Comparator<Card> {
     public int compare(Card c1, Card c2) {
-        int sortedBySuit = new SortBySuit().compare(c1, c2);
+        int suitComparison = c1.getSuit().compareTo(c2.getSuit());
 
-        if (sortedBySuit == 0) {
-            return c1.compareTo(c2);
+        if (suitComparison != 0) {
+            return suitComparison;
         }
 
-        return sortedBySuit;
+        return Integer.compare(c1.getValue(), c2.getValue());
     }
 }
