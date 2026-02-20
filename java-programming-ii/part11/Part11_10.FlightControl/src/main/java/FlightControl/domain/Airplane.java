f99025c16bbfc46e9a4336b45c4962d3c1b00176
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-20 09:28:57 -0300

    Part11_10: Create Airplane class in domain package

---
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/domain/Airplane.java
@@ -0,0 +1,16 @@
+package FlightControl.domain;
+
+public class Airplane {
+    private String ID;
+    private int capacity;
+
+    public Airplane(String ID, int capacity) {
+        this.ID = ID;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID + " (" + this.capacity + " capacity)";
+    }
+}
