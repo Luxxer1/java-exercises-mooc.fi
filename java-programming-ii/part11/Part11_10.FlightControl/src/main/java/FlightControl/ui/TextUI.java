d4fc3d0851ef127dd75ca857197f9096727151ec
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-20 08:21:52 -0300

    Part11_10: Create startAssetControl method

--- java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/ui/TextUI.java
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/ui/TextUI.java
@@ -10,6 +10,28 @@ public class TextUI {
     }
 
     public void start() {
+        startAssetControl();
+    }
 
+    private void startAssetControl() {
+        while (true) {
+            System.out.println("Choose an action:");
+            System.out.println("[1] Add an airplane");
+            System.out.println("[2] Add a flight");
+            System.out.println("[x] Exit Airport Asset Control");
+            String answer = scanner.nextLine();
+
+            if (answer.equals("1")) {
+
+            } else if (answer.equals("2")) {
+
+            } else if (answer.equals("X")) {
+                break;
+            }
+        }
     }
 }
+
+
+
+
