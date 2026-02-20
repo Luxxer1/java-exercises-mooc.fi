62927e2e874301e6b4268eba7f25bb252f64b216
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-20 07:59:51 -0300

    Part11_10: Create TextUI class

---
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/ui/TextUI.java
@@ -0,0 +1,15 @@
+package FlightControl.ui;
+
+import java.util.Scanner;
+
+public class TextUI {
+    Scanner scanner;
+
+    public TextUI(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void start() {
+
+    }
+}
