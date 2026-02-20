038e7511222d08dc8fa110b222e8a12def47d9fe
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-20 10:21:59 -0300

    Part11_10: Create addAirplane method in FlightControl

--- java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/logic/FlightControl.java
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/logic/FlightControl.java
@@ -1,5 +1,22 @@
 package FlightControl.logic;
 
-public class FlightControl {
+import java.util.Map;
+import java.util.HashMap;
+import FlightControl.domain.*;
 
+public class FlightControl {
+    Map<String, Airplane> airplanes;
+    Map<String, Place> places;
+    Map<String, Flight> flights;
+
+    public FlightControl() {
+        this.airplanes = new HashMap<>();
+        this.places = new HashMap<>();
+        this.flights = new HashMap<>();
+    }
+
+    public void addAirplane(String ID, int capacity) {
+        // Handle message or change to putIfAbsent
+        airplanes.put(ID, new Airplane(ID, capacity));
+    }
 }
