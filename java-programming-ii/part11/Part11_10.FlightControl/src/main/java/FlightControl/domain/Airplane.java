a1c3f4cb6c25f3e8b44306f3afb1c5bffece7ba4
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-25 10:47:57 -0300

    Part11_10: Implement Airplane variables and toString method

--- java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/domain/Airplane.java
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/domain/Airplane.java
@@ -1,6 +1,16 @@
 package FlightControl.domain;
 
 public class Airplane {
-    String ID;
-    int capacity;
+    private String ID;
+    private int capacity;
+
+    public Airplane(String ID, int capacity) {
+        this.ID = ID;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return ID + " (" + capacity + " capacity)";
+    }
 }
