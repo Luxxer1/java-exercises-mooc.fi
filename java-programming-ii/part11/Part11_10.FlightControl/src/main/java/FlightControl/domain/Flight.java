c8aba8fb498b607e241dd5fb5fe4ff6a7900c0a5
Lucas Bastos <lfsouzabastos@gmail.com>
2026-02-25 10:49:57 -0300

    Part11_10: Implement Flight variables and toString method

--- java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/domain/Flight.java
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/domain/Flight.java
@@ -1,4 +1,18 @@
 package FlightControl.domain;
 
 public class Flight {
+    Airplane airplane;
+    Place departurePlace;
+    Place targetPlace;
+
+    public Flight(Airplane airplane, Place departurePlace, Place targetPlace) {
+        this.airplane = airplane;
+        this.departurePlace = departurePlace;
+        this.targetPlace = targetPlace;
+    }
+
+    @Override
+    public String toString() {
+        return airplane + " (" + departurePlace + "-" + targetPlace + ")";
+    }
 }
