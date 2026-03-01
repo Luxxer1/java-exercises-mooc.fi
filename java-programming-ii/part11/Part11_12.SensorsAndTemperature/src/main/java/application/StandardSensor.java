e677aa4f622ba7c6e756cd4ebe43060a563e2ea7
Lucas Bastos <lfsouzabastos@gmail.com>
2026-03-01 14:11:44 -0300

    Part11_12: Remove unused variable

--- java-programming-ii/part11/Part11_12.SensorsAndTemperature/src/main/java/application/StandardSensor.java
+++ java-programming-ii/part11/Part11_12.SensorsAndTemperature/src/main/java/application/StandardSensor.java
@@ -2,11 +2,9 @@ package application;
 
 public class StandardSensor implements Sensor {
     private int sensorValue;
-    private boolean status;
 
     public StandardSensor(int sensorValue) {
         this.sensorValue = sensorValue;
-        this.status = true;
     }
 
     public boolean isOn() {
