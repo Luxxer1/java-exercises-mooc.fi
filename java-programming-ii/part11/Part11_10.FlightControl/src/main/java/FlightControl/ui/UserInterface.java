a466fa607251141aa2e33daf224b5d725f91de9d
Lucas Bastos <lfsouzabastos@gmail.com>
2026-03-01 09:28:44 -0300

    Part11_10: Create startAssetControl and addAirplane methods

--- java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/ui/UserInterface.java
+++ java-programming-ii/part11/Part11_10.FlightControl/src/main/java/FlightControl/ui/UserInterface.java
@@ -1,4 +1,44 @@
 package FlightControl.ui;
 
+import FlightControl.logic.FlightControl;
+
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
+    private FlightControl flightControl;
+
+    public UserInterface() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        startAssetControl();
+    }
+
+    public void startAssetControl() {
+        while (true) {
+            System.out.println("Choose an action:\n" +
+                    "[1] Add an airplane\n" +
+                    "[2] Add a flight\n" +
+                    "[x] Exit Airport Asset Control");
+
+            String input = scanner.nextLine();
+            if (input.equals("1")) {
+                addAirplane();
+            } else if (input.equals("2")) {
+
+            } else if (input.equals("x")) {
+
+            }
+        }
+    }
+
+    public void addAirplane() {
+        System.out.println("Give the airplane id: ");
+        String id = scanner.nextLine();
+        System.out.println("Give the airplane capacity: ");
+        int capacity = Integer.parseInt(scanner.nextLine());
+        flightControl.addAirplane(id, capacity);
+    }
 }
