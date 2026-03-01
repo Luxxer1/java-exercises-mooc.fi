53174b1fd68dc3e5f75f7537c871c177f4c63838
Lucas Bastos <lfsouzabastos@gmail.com>
2026-03-01 14:25:36 -0300

    Part11_12: Create TemperatureSensor

---
+++ java-programming-ii/part11/Part11_12.SensorsAndTemperature/src/main/java/application/TemperatureSensor.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Random;
+
+public class TemperatureSensor implements Sensor {
+    private boolean status;
+
+    public TemperatureSensor() {
+        this.status = false;
+    }
+
+    public boolean isOn() {
+        return status;
+    }
+
+    public void setOn() {
+
+    }
+
+    public void setOff() {
+
+    }
+
+    public int read() {
+        if (status) {
+            return new Random().nextInt(61) - 30;
+        } else {
+            throw new IllegalStateException("Can't read from a sensor that is off");
+        }
+    }
+}
