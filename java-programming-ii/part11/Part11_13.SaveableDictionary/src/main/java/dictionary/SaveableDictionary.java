75f13acbf6ae07aab05b644a04c22fc79e57ea88
Lucas Bastos <lfsouzabastos@gmail.com>
2026-03-10 10:17:55 -0300

    Part11_13: Create SaveableDictionary and methods add and translate

---
+++ java-programming-ii/part11/Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -0,0 +1,20 @@
+package dictionary;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class SaveableDictionary {
+    Map<String, String> dictionary;
+
+    public SaveableDictionary() {
+        this.dictionary = new HashMap<>();
+    }
+
+    public void add(String word, String translation) {
+        dictionary.putIfAbsent(word, translation);
+    }
+
+    public String translate(String word) {
+        return dictionary.get(word);
+    }
+}
