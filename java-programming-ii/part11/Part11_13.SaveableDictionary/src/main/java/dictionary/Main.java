d1cbc5c523d5e01056b0a296bba6f8235f6debed
Lucas Bastos <lfsouzabastos@gmail.com>
2026-03-10 11:36:45 -0300

    Part11_13: Add code to test the program

--- java-programming-ii/part11/Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
+++ java-programming-ii/part11/Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
@@ -2,7 +2,7 @@ package dictionary;
 
 public class Main {
     public static void main(String[] args) {
-        SaveableDictionary dictionary = new SaveableDictionary();
+        SaveableDictionary dictionary = new SaveableDictionary("words.txt");
         dictionary.add("apina", "monkey");
         dictionary.add("banaani", "banana");
         dictionary.add("apina", "apfe");
