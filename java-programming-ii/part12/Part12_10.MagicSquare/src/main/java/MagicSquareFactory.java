037612332312833128b456423da915f7f823fc5c
Lucas Bastos <lfsouzabastos@gmail.com>
2026-04-15 11:26:41 -0300

    Part12_10: Create initial implementation of createMagicSquare

--- java-programming-ii/part12/Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ java-programming-ii/part12/Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -2,11 +2,21 @@
 public class MagicSquareFactory {
 
     public MagicSquare createMagicSquare(int size) {
-
         MagicSquare square = new MagicSquare(size);
+        if (size % 2 == 0) {
+            return square;
+        }
 
-        // implement the creation of a magic square with the Siamese method algorithm here
+        int x = square.getWidth() / 2;
+        int y = 0;
+        int value = 1;
+
+        square.placeValue(x, y, value);
+
+        while (!square.isMagicSquare()) {
+
+        }
+
         return square;
     }
-
 }
