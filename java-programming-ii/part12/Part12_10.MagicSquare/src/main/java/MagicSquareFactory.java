45d0fd5f4b04f9a2cfbe487912481ba8e1f40203
Lucas Bastos <lfsouzabastos@gmail.com>
2026-04-17 11:04:51 -0300

    Part12_10: Implement createMagicSquare method

--- java-programming-ii/part12/Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ java-programming-ii/part12/Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -9,12 +9,28 @@ public class MagicSquareFactory {
 
         int x = square.getWidth() / 2;
         int y = 0;
+
         int value = 1;
+        int lastNumber = size * size;
 
         square.placeValue(x, y, value);
 
-        while (!square.isMagicSquare()) {
+        while (value <= lastNumber) {
+            value += 1;
+            int nextX = x + 1;
+            int nextY = y - 1;
 
+            if (nextX >= square.getWidth()) nextX = 0;
+            if (nextY < 0) nextY = square.getHeight() - 1;
+
+            if (square.readValue(nextX, nextY) != 0) {
+                nextX = x;
+                nextY = y + 1;
+            }
+
+            square.placeValue(nextX, nextY, value);
+            x = nextX;
+            y = nextY;
         }
 
         return square;
